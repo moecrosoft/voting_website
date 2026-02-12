54a42aa14803b4f4d0266481e58505d46fe7eb94
Vicky Yang <vicz.yang@gmail.com>
2026-02-12 14:42:52 +0800

    finished login page

---
+++ voting_website/src/components/navBar.jsx
@@ -0,0 +1,8 @@
+export function Results() {
+  return (
+    <div className="max-w-7xl mx-auto p-8">
+      <h1 className="text-red-600 mb-8">Results</h1>
+      <p className="text-gray-600">Results page content will be displayed here.</p>
+    </div>
+  );
+}
